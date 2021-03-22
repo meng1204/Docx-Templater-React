e889d305af413525330cd637515275c839776a92
Nico Chen <meng8859@gmail.com>
2021-03-21 22:59:05 -0700

    Init home layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Home} from "./pages/Home"
+import './assets/css/App.css'
+import 'antd/dist/antd.css';
+import { Layout} from 'antd';
+const { Header, Footer, Content } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = class App extends React.Component {
+  render() {
+    return (
+      <Layout className="layout">
+      <Header>
+        <div className="title"> Docs Templater </div>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content"><Home/></div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Docs Templater ©2021 Created by Nico Chen</Footer>
+    </Layout>
+    );
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  // <React.StrictMode>
+    <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
